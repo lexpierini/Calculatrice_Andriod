9160c9000426292d9ca47383b8aa074342439302
lexpierini <alex_pierini@hotmail.com>
2020-04-03 23:39:11 -0400

    Ajout de contenu sur l'écran d'accueil.

---
+++ app/src/main/java/com/example/apierini_pnm_tp3/CalculatriceActivity.java
@@ -0,0 +1,14 @@
+package com.example.apierini_pnm_tp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
