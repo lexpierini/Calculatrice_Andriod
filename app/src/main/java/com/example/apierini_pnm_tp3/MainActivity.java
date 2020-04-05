4e6c5ea1902af88298526e88a13d04f3e13c18e5
lexpierini <alex_pierini@hotmail.com>
2020-04-05 18:19:03 -0400

    Mise en place d'un Splash Screen de 5 secondes entre MainActivity et CalculatriceActivity.

--- app/src/main/java/com/example/apierini_pnm_tp3/MainActivity.java
+++ app/src/main/java/com/example/apierini_pnm_tp3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.apierini_pnm_tp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), CalculatriceActivity.class));
+                finish();
+            }
+        }, 5000);
     }
 }
